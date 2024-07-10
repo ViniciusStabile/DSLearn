938057977c31102cbe77d5172310ff34b27d59e5
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-10 17:59:40 -0300

    First commit

---
+++ src/test/java/DSLearn/DsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package DSLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
