5fb125987950d60d85e14f1b4257732b1a7754aa
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-10 18:25:42 -0300

    Implemented many-to-many relationship between User and role entities

--- src/main/java/DSLearn/entities/User.java
+++ src/main/java/DSLearn/entities/User.java
@@ -5,9 +5,13 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,6 +28,10 @@ public class User implements Serializable{
 	private String email;
 	private String password;
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", 
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles;
 
 	public User() {
