e1d1f202cf0fc081c7637d66bfc3389ea1e73fcd
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-10 18:13:26 -0300

    Update role entity

--- src/main/java/DSLearn/entities/Role.java
+++ src/main/java/DSLearn/entities/Role.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -12,6 +15,8 @@ public class Role implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String authority;
 	
