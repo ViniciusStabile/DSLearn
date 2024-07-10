07be94dfa199441b08ed11ac9a2a6e1fdb94251c
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-10 18:08:37 -0300

    Role entity

---
+++ src/main/java/DSLearn/entities/Role.java
@@ -0,0 +1,62 @@
+package DSLearn.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String authority;
+	
+	public Role() {
+		
+	}
+
+	public Role(Long id, String authority) {
+		this.id = id;
+		this.authority = authority;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
