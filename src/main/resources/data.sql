14aa24f214ee5ad58bead34a0142da74591ed7bc
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-11 19:51:17 -0300

    Enrollment seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,8 +15,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES ('java', 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png', 'https://cdn.iconscout.com/icon/free/png-256/free-java-file-51-775447.png');
 
-INSERT INTO tb_offer(edition,start_Moment,end_Moment, course_id) VALUES ('1.0', '2020-07-13T20:50:07.12345Z', '2021-07-13T20:50:07.12345Z', '1');
-INSERT INTO tb_offer(edition,start_Moment,end_Moment, course_id) VALUES ('1.0', '2021-07-13T20:50:07.12345Z', '2022-07-13T20:50:07.12345Z', '1');
+INSERT INTO tb_offer(edition,start_Moment,end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T23:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T23:00:00Z', '1');
+INSERT INTO tb_offer(edition,start_Moment,end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-11-20T23:00:00Z', TIMESTAMP WITH TIME ZONE '2022-11-20T23:00:00Z', '1');
 
 INSERT INTO tb_resource(title, description, position, img_Url, type, offer_id) VALUES ('Trilha html', 'Trilha do curso',1,'https://storage.needpix.com/rsynced_images/java-2327538_1280.png', 1, 1);
 INSERT INTO tb_resource(title, description, position, img_Url, type, offer_id) VALUES ('Trilha html 2', 'Trilha do curso 2',2,'https://storage.needpix.com/rsynced_images/java-2327538_1280.png', 1, 2);
@@ -24,3 +24,8 @@ INSERT INTO tb_resource(title, description, position, img_Url, type, offer_id) V
 INSERT INTO tb_section(title,description, position, img_Url, resource_id, prerequisite_id) VALUES ('Capitulo 1','Neste capitulo vamos começar', 1, 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png',1,null);
 INSERT INTO tb_section(title,description, position, img_Url, resource_id, prerequisite_id) VALUES ('Capitulo 2','Neste capitulo vamos continuar', 2, 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png',1,1);
 INSERT INTO tb_section(title,description, position, img_Url, resource_id, prerequisite_id) VALUES ('Capitulo 3','Neste capitulo vamos terminar', 3, 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png',1,2);
+
+INSERT INTO tb_enrollment(user_id, offer_id,enroll_Moment,refund_Moment,available,only_Update) VALUES (1,1,TIMESTAMP WITH TIME ZONE '2020-11-20T23:00:00Z', null, true, false);
+INSERT INTO tb_enrollment(user_id, offer_id,enroll_Moment,refund_Moment,available,only_Update) VALUES (2,1,TIMESTAMP WITH TIME ZONE '2020-12-20T23:00:00Z', null, true, false);
+
+
