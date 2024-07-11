07a1d89f3862753fa5966b051ea68d2a4f1398e8
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-11 18:53:55 -0300

    Section entity

--- src/main/java/DSLearn/entities/Section.java
+++ src/main/java/DSLearn/entities/Section.java
@@ -1,26 +1,22 @@
 package DSLearn.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import DSLearn.entities.enums.ResourceType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_resource")
-public class Resource implements Serializable {
+@Table(name = "tb_section")
+public class Section implements Serializable {
 
-private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -28,27 +24,28 @@ private static final long serialVersionUID = 1L;
 	private String description;
 	private Integer position;
 	private String imgUrl;
-	private ResourceType type;
 	
 	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
-	
-	@OneToMany(mappedBy = "resource")
-	private List<Section> section = new ArrayList<>();
-	
-	public Resource() {
+	@JoinColumn(name = "resource_id")
+	private Resource resource;
+
+	@ManyToOne
+	@JoinColumn(name = "prerequisite_id")
+	private Section prerequisite;
+
+	public Section() {
+
 	}
 
-	public Resource(Long id, String title, String description, Integer position, String imgUrl, ResourceType type,
-			Offer offer) {
+	public Section(Long id, String title, String description, Integer position, String imgUrl, Resource resource,
+			Section prerequisite) {
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.position = position;
 		this.imgUrl = imgUrl;
-		this.type = type;
-		this.offer = offer;
+		this.resource = resource;
+		this.prerequisite = prerequisite;
 	}
 
 	public Long getId() {
@@ -91,20 +88,20 @@ private static final long serialVersionUID = 1L;
 		this.imgUrl = imgUrl;
 	}
 
-	public ResourceType getType() {
-		return type;
+	public Resource getResource() {
+		return resource;
 	}
 
-	public void setType(ResourceType type) {
-		this.type = type;
+	public void setResource(Resource resource) {
+		this.resource = resource;
 	}
 
-	public Offer getOffer() {
-		return offer;
+	public Section getPrerequisite() {
+		return prerequisite;
 	}
 
-	public void setOffer(Offer offer) {
-		this.offer = offer;
+	public void setPrerequisite(Section prerequisite) {
+		this.prerequisite = prerequisite;
 	}
 
 	@Override
@@ -120,9 +117,8 @@ private static final long serialVersionUID = 1L;
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Resource other = (Resource) obj;
+		Section other = (Section) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,9 +15,12 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES ('java', 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png', 'https://cdn.iconscout.com/icon/free/png-256/free-java-file-51-775447.png');
 
-
 INSERT INTO tb_offer(edition,start_Moment,end_Moment, course_id) VALUES ('1.0', '2020-07-13T20:50:07.12345Z', '2021-07-13T20:50:07.12345Z', '1');
 INSERT INTO tb_offer(edition,start_Moment,end_Moment, course_id) VALUES ('1.0', '2021-07-13T20:50:07.12345Z', '2022-07-13T20:50:07.12345Z', '1');
 
-
 INSERT INTO tb_resource(title, description, position, img_Url, type, offer_id) VALUES ('Trilha html', 'Trilha do curso',1,'https://storage.needpix.com/rsynced_images/java-2327538_1280.png', 1, 1);
+INSERT INTO tb_resource(title, description, position, img_Url, type, offer_id) VALUES ('Trilha html 2', 'Trilha do curso 2',2,'https://storage.needpix.com/rsynced_images/java-2327538_1280.png', 1, 2);
+
+INSERT INTO tb_section(title,description, position, img_Url, resource_id, prerequisite_id) VALUES ('Capitulo 1','Neste capitulo vamos começar', 1, 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png',1,null);
+INSERT INTO tb_section(title,description, position, img_Url, resource_id, prerequisite_id) VALUES ('Capitulo 2','Neste capitulo vamos continuar', 2, 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png',1,1);
+INSERT INTO tb_section(title,description, position, img_Url, resource_id, prerequisite_id) VALUES ('Capitulo 3','Neste capitulo vamos terminar', 3, 'https://storage.needpix.com/rsynced_images/java-2327538_1280.png',1,2);
