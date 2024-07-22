ae4f082cb02cbda26f64d72323f596ceea1949d5
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-07-22 18:55:41 -0300

    UserRepository, UserDetailsProjection

---
+++ src/main/java/DSLearn/projections/UserDetailsProjection.java
@@ -0,0 +1,10 @@
+package DSLearn.projections;
+
+public interface UserDetailsProjection {
+
+	String getName();
+	String getEmail();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
---
+++ src/main/java/DSLearn/repositories/UserRepository.java
@@ -0,0 +1,23 @@
+package DSLearn.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import DSLearn.entities.User;
+import DSLearn.projections.UserDetailsProjection;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	User findByEmail(String emaill);
+
+	@Query(nativeQuery = true, value = """
+				SELECT tb_user.name, tb_user.email, tb_user.password, tb_role.id AS roleId, tb_role.authority
+				FROM tb_user
+				INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+				INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+				WHERE tb_user.email = :email
+			""")
+	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+}
