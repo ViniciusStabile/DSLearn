086ff88d4cdf710144fe54c8db60adbcc792ce77
Vinicius Stabile <viniciusstabile@yahoo.com.br>
2024-08-14 18:55:20 -0300

    ContentDTO

---
+++ src/main/java/DSLearn/DTO/ContentDTO.java
@@ -0,0 +1,42 @@
+package DSLearn.DTO;
+
+import DSLearn.entities.Content;
+
+public class ContentDTO extends LessonDTO {
+
+	private String textContent;
+	private String videoUri;
+
+	public ContentDTO() {
+		super();
+	}
+
+	public ContentDTO(Long id, String title, Integer position, String textContent, String videoUri) {
+		super(id, title, position);
+		this.textContent = textContent;
+		this.videoUri = videoUri;
+	}
+
+	public ContentDTO(Content entity) {
+		super(entity.getId(), entity.getTitle(), entity.getPosition());
+		this.textContent = entity.getTextContent();
+		this.videoUri = entity.getVideoUrl();
+	}
+
+	public String getTextContent() {
+		return textContent;
+	}
+
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
+
+	public String getVideoUri() {
+		return videoUri;
+	}
+
+	public void setVideoUri(String videoUri) {
+		this.videoUri = videoUri;
+	}
+
+}
